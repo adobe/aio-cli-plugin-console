2f75e592e72ae661d8c18272d057bc4de00123b6
Shazron Abdullah <shazron@gmail.com>
2020-05-14 16:57:48 +0800

    fix: remove existing console commands to prep for new implementation

--- test/commands/console/index.test.js
+++ test/commands/console/index.test.js
@@ -1,9 +1,8 @@
 /*
-Copyright 2018 Adobe. All rights reserved.
+Copyright 2020 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software distributed under
 the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 OF ANY KIND, either express or implied. See the License for the specific language
@@ -11,24 +10,16 @@ governing permissions and limitations under the License.
 */
 
 const ConsoleCommand = require('../../../src/commands/console')
-const ListIntegrationsCommand = require('../../../src/commands/console/list-integrations')
+const { Command } = require('@oclif/command')
 
 describe('ConsoleCommand (index) ', () => {
-  test('extends ListIntegrationsCommand', async () => {
-    expect(ConsoleCommand.run).toEqual(ListIntegrationsCommand.run)
+  test('extends oclif Command', async () => {
+    expect(ConsoleCommand.run).toEqual(Command.run)
   })
 })
 
 describe('basic command properties', () => {
   test('has a description', () => {
     expect(ConsoleCommand.description).toBeDefined()
-  })
-
-  test('has a examples', () => {
-    expect(ConsoleCommand.examples).toBeDefined()
-  })
-
-  test('has flags', () => {
-    expect(ConsoleCommand.flags).toBeDefined()
   })
 })
