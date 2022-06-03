5844c83454d7b9516b41bb7c666ddaadacdce8b9
Mark Adamcin <adamcin@adobe.com>
2022-06-02 17:17:04 -0700

    calculate cert fingerprint and check for existence before upload

--- src/commands/console/publickey/upload.js
+++ src/commands/console/publickey/upload.js
@@ -16,6 +16,9 @@ const { cli } = require('cli-ux')
 const { CONFIG_KEYS } = require('../../../config')
 const LibConsoleCLI = require('@adobe/aio-cli-lib-console')
 const aioConsoleLogger = require('@adobe/aio-lib-core-logging')('@adobe/aio-cli-plugin-console:workspace:bind', { provider: 'debug' })
+const forge = require('node-forge')
+const pki = forge.pki
+const asn1 = forge.asn1
 
 class UploadAndBindCommand extends ConsoleCommand {
   async run () {
@@ -59,19 +62,32 @@ class UploadAndBindCommand extends ConsoleCommand {
       const project = consoleConfig.project
       const workspace = project.workspace
 
-      const bindings = await this.consoleCLI.uploadAndBindCertificate(
-        orgId,
-        project,
-        workspace,
-        args.publicKey
-      )
+      const certPemString = fs.readFileSync(publicKey)
+      const fingerprint = this.computeFingerprint(certPemString)
+
+      const existing = await this.consoleCLI.getBindingsForWorkspace(orgId, project, workspace)
+
+      const bindings = []
+      const found = existing.find((value) => value.certificateFingerprint === fingerprint)
+      if (found) {
+        aioConsoleLogger.debug(`Found existing binding matching publicKey fingerprint ${fingerprint}. Will skip upload.`)
+        bindings.push(found)
+      } else {
+        const created = await this.consoleCLI.uploadAndBindCertificate(
+          orgId,
+          project,
+          workspace,
+          args.publicKey
+        )
+        bindings.push(created)
+      }
 
       if (flags.json) {
-        this.printJson([bindings])
+        this.printJson(bindings)
       } else if (flags.yml) {
-        this.printYaml([bindings])
+        this.printYaml(bindings)
       } else {
-        this.printResults([bindings])
+        this.printResults(bindings)
       }
       return bindings
     } catch (err) {
@@ -80,6 +96,24 @@ class UploadAndBindCommand extends ConsoleCommand {
     } finally {
       LibConsoleCLI.cleanStdOut()
     }
+  }
+
+  /**
+   * Computes the SHA-1 digest of the entire DER-encoded x.509 certificate
+   * contained in the provided PEM-encoded string, which gives the same result as
+   * using "openssl x509 -fingerprint", except without delimeters and in
+   * all lowercase.
+   *
+   * @param {string} certPemString PEM-encoded sting containing x509 certificate
+   * @returns {string} x509 fingerprint
+   */
+  computeFingerprint (certPemString) {
+    const cert = pki.certificateFromPem(certPemString, false, false)
+    const bytes = asn1.toDer(pki.certificateToAsn1(cert)).getBytes()
+    const md = forge.md.sha1.create()
+    md.start()
+    md.update(bytes)
+    return md.digest().toHex()
   }
 
   printResults (bindings) {
